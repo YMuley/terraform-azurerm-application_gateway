bc698628e8f9fbfdb0c6fdf078472cffc94472b8
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-12-28 17:55:27 +0530

    code updated

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "azurerm_application_gateway" "application_gateway" {
       for_each = length([for identity_key in each.value.identity : keys(identity_key)]) > 0 ? each.value.identity : []    #length(keys(each.value.identity))
       content {
       type = identity.value.type
-      identity_ids = var.user_assigned_identity_output[tostring(element(identity.value.identity_ids,0))].id
+      identity_ids = var.user_assigned_identity_output[toset(element(identity.value.identity_ids,0))].id
       }
        
     }
